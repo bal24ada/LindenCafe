c24523480b8ae6f206c5dca3fc07ac13c47d3213
Ada <ada@ada.balsecen@student.htl-rankweil.at>
2026-06-02 16:24:16 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LindenCafe_BalsecenAda_KasperJamie
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
