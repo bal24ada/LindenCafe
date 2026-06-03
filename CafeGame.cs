0567f514f41fef65429074b564c229fd47124f53
Ada <ada@ada.balsecen@student.htl-rankweil.at>
2026-06-03 12:30:51 +0200

    Start with die CafeGame Class

---
+++ CafeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace LindenCafe_BalsecenAda_KasperJamie
+{
+    class CafeGame
+    {
+        private int money { get; set; }
+        private int cafeLevel { get; set; }
+        private List<Customer> customers = new List<Customer>();
+        private List<Table> tables = new List<Table>();
+
+
+    }
+}
