3e4b7392718af1e4cc1f2a2779d98f60f79dbc67
Ada <ada@ada.balsecen@student.htl-rankweil.at>
2026-06-16 14:36:53 +0200

    CafeGame Class completeted

--- CafeGame.cs
+++ CafeGame.cs
@@ -14,6 +14,63 @@ namespace LindenCafe_BalsecenAda_KasperJamie
         private List<Customer> customers = new List<Customer>();
         private List<Table> tables = new List<Table>();
 
+        private CafeGame()
+        {
+            money = 100;
+            cafeLevel = 1;
+        }
+
+        private CafeGame(int money, int cafeLevel)
+        {
+            this.money = money;
+            this.cafeLevel = cafeLevel;
+        }
+
+        public void StartGame()
+        {
+            Table table1 = new Table();
+            tables.Add(table1);
+            Table table2 = new Table();
+            tables.Add(table2);
+            Table table3 = new Table();
+            tables.Add(table3);
+        }
+        public void SpawnCustomer()
+        {
+            Customer customer = new Customer();
+            customers.Add(customer);
+
+        }
+
+        public void AddMoney(int amount)
+        {
+            money += amount;
+
+        }
+        public void UpgradeCafe()
+        {
+            if (money > 200)
+            {
+                money -= 200;
+                cafeLevel = cafeLevel + 1;
+
+            }
+            else
+            {
+                Console.WriteLine("Du hast nicht genügend Geld für ein Upgrade!");
+            }
+
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Cafe level: {cafeLevel}");
+            Console.WriteLine($"Money amount: {money}");
+        }
+
+
+
 
     }
+
+
 }
